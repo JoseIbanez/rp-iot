c495b44a01654f8b63c4dfc87b7d9fc2520a71dc
José Ibañez <ibanez.j@gmail.com>
2017-07-27 09:09:22 +0200

    cron

--- p07-sns-temp/get_measure.sh
+++ p07-sns-temp/get_measure.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+home="/home/pi/Projects/rp-iot/p07-sns-temp"
+cd $home
 
 probes=`ls -1 /etc/rp-iot/probe*.yaml`
 for i in $probes; do 
-cat $i
-sleep 10
+    ./temp.py -c $i
+    sleep 10
 done
