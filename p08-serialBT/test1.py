8a79d4ad396ddeb84b59fdd18157ac0d6b9d053d
Jose Ibanez <ibanez.j@gmail.com>
2017-11-30 10:40:40 +0000

    Serial basic test

--- p08-serialBT/test1.py
+++ p08-serialBT/test1.py
@@ -3,11 +3,29 @@ import serial
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-    port='/dev/rfcomm0',
-    baudrate=9600)
+    port='/dev/rfcomm1',
+    baudrate=9600,
+    timeout=5)
 
-ser.isOpen()
+if ser.isOpen():
+    print "isOpen: True"
+else:
+    print "isOpen: False"
 
 print ser.portstr       # check which port was really used
+
+ser.write("0;0000")
+ans = ser.readline()
+if ans == "0000":
+   print "Test port: OK"
+else:
+   print "Test port: Failed"
+
+
 ser.write("10;1100")      # write a string
-ser.close()  
+ans = ser.readline()
+
+
+print ans
+ser.close()
+
