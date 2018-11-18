253a0af76c5c96bcb47a335f9a7fda1818048e98
Jose Ibanez <ibanez.j@gmail.com>
2018-11-18 23:46:22 +0100

    spaces

--- p10-serialBT/portServer.py
+++ p10-serialBT/portServer.py
@@ -11,6 +11,7 @@ cmdList = []
 serStatus = 0
 
 
+
 def on_new_client(clientsocket,addr):
     while True:
         try:
@@ -21,12 +22,10 @@ def on_new_client(clientsocket,addr):
         if len(msg) > 0:
             cmdList.append(msg)
 
-        #do some checks and if msg == someWeirdSignal: break:
         print addr, ' Rec ', msg
         msg = "serStatus: "+str(serStatus)
         time.sleep(1)
 
-        #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
         try:
             clientsocket.send(msg)
         except:
@@ -45,12 +44,12 @@ def serialServer():
 
     while True:
 
-	if lastCmd > timeToSleep and len(cmdList) == 0:
+        if lastCmd > timeToSleep and len(cmdList) == 0:
             print "z"
-	    time.sleep(2)
-	    continue
- 	elif lastCmd > timeToSleep and len(cmdList) > 0:
-	    print "Wakeup"
+            time.sleep(2)
+            continue
+        elif lastCmd > timeToSleep and len(cmdList) > 0:
+            print "Wakeup"
             lastCmd = 0
 
 
@@ -80,8 +79,8 @@ def serialServer():
         lastCmd  = lastCmd + 1
         print "."
 
-	if lastAnswer > 20:
-	    serStatus = 0
+        if lastAnswer > 20:
+            serStatus = 0
             lastAnswer = 0
             ser.close()
             print "Reset"
@@ -98,13 +97,13 @@ def serialServer():
 
 
         if lastAnswer > 10:
-            #Just for test
+            #Check connection status
             serStatus = 1
-	    print ">>STATUS"
-	    try:
+            print ">>STATUS"
+            try:
             	ser.write("STATUS")
-	    except:
-		pass
+            except:
+                pass
             continue
 
 
@@ -117,14 +116,7 @@ def serialServer():
 
 
 
-
-#s = socket.socket()         # Create a socket object
-#host = socket.gethostname() # Get local machine name
-#port = 50000                # Reserve a port for your service.
-
-
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 
 try:
     os.remove("/tmp/channel0")
@@ -140,15 +132,13 @@ thread.start_new_thread(serialServer,())
 print 'Waiting for clients...'
 
 s.bind("/tmp/channel0")
-#s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 
-#print 'Got connection from', addr
+
 while True:
     c, addr = s.accept()     # Establish connection with client.
     thread.start_new_thread(on_new_client,(c,addr))
-    #Note it's (addr,) not (addr) because second parameter is a tuple
-    #Edit: (c,addr)
-    #that's how you pass arguments to functions when creating new threads using thread module.
+
+
 s.close()
 
