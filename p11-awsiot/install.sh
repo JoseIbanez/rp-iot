d40c5106cf6eafbc9249df07719315fb5663d383
Jose Ibanez <ibanez.j@gmail.com>
2018-11-18 18:05:31 +0100

    bash and socket

---
+++ p11-awsiot/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# https://github.com/aws/aws-iot-device-sdk-python
+# https://docs.aws.amazon.com/iot/latest/developerguide/managing-device-certs.html#server-authentication
+# https://www.hackster.io/ben-eagan/alexa-controlled-leds-through-arduino-yun-ac1171
+
+sudo apt-get install python-pip
+
+pip install awscli AWSIoTPythonSDK
+mkdir -p ~/.secrets/iot
+wget https://www.amazontrust.com/repository/AmazonRootCA1.pem -P ~/.secrets/iot/
+wget https://www.amazontrust.com/repository/AmazonRootCA2.pem -P ~/.secrets/iot/
+
+
+aws iot-data get-thing-shadow    --thing-name v01 o.txt && cat o.txt
+aws iot-data update-thing-shadow --thing-name v01 --payload '{"state":{"desired":{"color":"green"}}}' o.log
+aws iot-data update-thing-shadow --thing-name h02-001 --payload '{"state":{"desired":{"property":33}}}' o.log
+
+
+
+ffmpeg -i /dev/video0 -f alsa -ac 1 -i plughw:1,0 -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental \
+    -f flv rtmp://a.rtmp.youtube.com/live2/$CHANNELID
---
+++ p11-awsiot/myThing.py
@@ -0,0 +1,175 @@
+'''
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import logging
+import time
+import json
+#import argparse
+from os.path import expanduser
+import yaml
+import socket
+#import thread
+import sys
+import subprocess
+
+# Custom Shadow callback
+def customShadowCallback_Delta(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    print("++++++++DELTA++++++++++")
+    print("Payload: "+ str(payload))
+    print("responseStatus: "+str(responseStatus))
+    print("token: "+str(token))
+    payloadDict = json.loads(payload)
+    print("state: " + str(payloadDict["state"]))
+    print("version: " + str(payloadDict["version"]))
+    print("+++++++++++++++++++++++\n\n")
+
+    try:
+        state = payloadDict["state"]
+    except:
+        print("No property received")
+        return
+
+    ansState = controlerMapping(state)
+    if not ansState:
+        return
+
+    try:
+        JSONPayload = {"state":{"reported": state }}
+        print("Reporting: "+str(JSONPayload))
+        deviceShadowHandler.shadowUpdate(json.dumps(JSONPayload), None, 5)
+        #time.sleep(5)
+        #JSONPayload = {"state":{"reported":{"property": 0 }}}
+        #deviceShadowHandler.shadowUpdate(json.dumps(JSONPayload), None, 5)
+    except:
+        print("report error")
+
+
+def controlerMapping(reqState):
+
+    retState = {}
+
+    for key in reqState:
+
+        #print str(config['actions'])
+        action = config['actions'].get(key)
+        print str(action)
+
+        if not action:
+            print "Command not defined"
+            continue
+
+        #request state, only valid for on/off devices
+        reqValue = reqState[key]
+        cmdType = action.get('type')
+
+        cmd  =  action.get(reqValue)
+
+        if not cmdType or not cmd:
+            print "Command format error"
+            continue
+
+        ###########################################
+        if cmdType == "bash":
+
+            print cmd
+            try:
+                subprocess.Popen(cmd.split())
+                retState[key] = reqState[key]
+            except:
+                print sys.exc_info()[0]
+
+
+        ###########################################
+        if cmdType == "socket":
+
+            sPath = action.get('socket')
+            print cmd + " > " + sPath
+            try:
+                s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                s.connect(sPath)
+                s.send(cmd)
+                data = s.recv(1024)
+                print("socket data: "+data)
+                retState[key] = reqState[key]
+            except Exception as e:
+                print "socket error "+str(e)
+
+            try:
+                s.close()
+            except:
+                pass
+
+
+    return retState
+
+
+
+
+
+
+
+# Read in config-file parameters
+configFile = "~/.secrets/iot/iot-config.yml"
+try:
+    with open(expanduser(configFile), 'r') as stream:
+        config = yaml.load(stream)
+except yaml.YAMLError as exc:
+        print exc
+
+host = config.get('host')
+rootCAPath = expanduser(config.get('rootCAPath'))
+certificatePath = expanduser(config.get('certificatePath'))
+privateKeyPath = expanduser(config.get('privateKeyPath'))
+thingName = config.get('thingName')
+clientId = config.get('clientId')
+port = 8883
+
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTShadowClient
+myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(clientId)
+myAWSIoTMQTTShadowClient.configureEndpoint(host, port)
+myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTShadowClient configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect to AWS IoT
+myAWSIoTMQTTShadowClient.connect()
+
+# Create a deviceShadow with persistent subscription
+deviceShadowHandler = myAWSIoTMQTTShadowClient.createShadowHandlerWithName(thingName, True)
+
+# Listen on deltas
+deviceShadowHandler.shadowRegisterDeltaCallback(customShadowCallback_Delta)
+
+
+# Loop forever
+while True:
+    time.sleep(1)
