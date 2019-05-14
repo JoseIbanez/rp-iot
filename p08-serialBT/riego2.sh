6844d997977978935073705d67bb18528fa918f8
Jose Ibanez <ibanez.j@gmail.com>
2019-05-14 21:22:18 +0000

    riego2

--- p08-serialBT/riego2.sh
+++ p08-serialBT/riego2.sh
@@ -10,9 +10,9 @@ cd $home
 python ./test4.py -port /dev/rfcomm1 -msg "5;0001"
 sleep 6
 
-python ./test4.py -port /dev/rfcomm1 -msg "18;1001"
+python ./test4.py -port /dev/rfcomm1 -msg "21;1001"
 sleep 30
-python ./test4.py -port /dev/rfcomm1 -msg "18;0101"
+python ./test4.py -port /dev/rfcomm1 -msg "15;0101"
 sleep 30
 
 python ./test4.py -port /dev/rfcomm1 -msg "900;0001"
